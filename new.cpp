ced84b19e92feac3f97f27c219da43895523f29a
ATHARVA YEOLA <114800045+Atha2445@users.noreply.github.com>
2023-08-08 17:16:57 +0530

    Add files via upload

---
+++ new.cpp
@@ -0,0 +1,11 @@
+#include <iostream.h>
+#include <conio.h>
+int main()
+{
+    int a,b,c;
+    cout<<"Enter any number";
+    cin>>a>>b;
+    c=a+b;
+    cout<<"Addition of "<<a<<" and "<<b<<"="<<c;
+    return 0;
+}
